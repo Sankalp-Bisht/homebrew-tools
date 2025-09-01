6947a6c8b0483a55b98c88aafad03dc923bdf783
Sankalp Singh Bisht <Sankalp.bisht@1mg.com>
2025-09-01 21:39:12 +0530

    feat: Added rb file with Homebrew formula

---
+++ Formula/automate-swiftui-accessibility.rb
@@ -0,0 +1,17 @@
+class SwiftuiAcc < Formula
+  desc "CLI for SwiftUI accessibility automation"
+  homepage "https://github.com/Sankalp-Bisht/SwiftUI-Accessibility-Automation"
+  version "1.0"
+
+  url "https://github.com/Sankalp-Bisht/SwiftUI-Accessibility-Automation/releases/download/1.0/swiftui-acc-arm64"
+  sha256 "6ca44da3cc255c3633ce2045cedefe6c554924120934de976639b7f8076d01cd"
+
+  def install
+    bin.install "swiftui-acc-arm64" => "swiftui-acc"
+    chmod 0555, bin/"swiftui-acc"
+  end
+
+  test do
+    assert_match "--help", shell_output("#{bin}/swiftui-acc --help")
+  end
+end
